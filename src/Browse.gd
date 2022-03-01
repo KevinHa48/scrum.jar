e1e0fb62c9fa71914f3e54c298b632da6dbbce85
liamsusername <lkpowp@gmail.com>
2022-03-01 13:30:38 -0500

    Added ability to type filepath

--- src/Browse.gd
+++ src/Browse.gd
@@ -1,6 +1,6 @@
 extends Control
 
-
+var filePath = ""
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -18,3 +18,13 @@ func _ready():
 
 func _on_BrowseBtn_pressed():
 	$NinePatchRect/VBoxContainer/UploadDlg.popup()
+	
+
+func _on_UploadDlg_file_selected(path):
+	$NinePatchRect/VBoxContainer/LineEdit.text = path
+	filePath = path
+
+
+func _on_LineEdit_text_changed(new_text):
+	filePath = new_text
+	print(filePath)
