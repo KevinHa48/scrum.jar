d29cbdd14e10fccc1ac765bb163f092257dba02f
KevinHa48 <kh220kh@gmail.com>
2022-03-03 02:06:10 -0500

    Added WIP for skybox color changing script

---
+++ src/skycolors.gd
@@ -0,0 +1,36 @@
+extends Gradient
+var r = 0.0; var rup = true;
+var g = 0.0; var gup = true;
+var b = 0.0; var bup = true;
+
+# Script should be added in the inspector mode and referenced.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func calcColors(delta):
+	if(r >= 1.0): rup = false;
+	elif(r <= 0.0): rup = true;
+	if(g >= 1.0): gup = false;
+	elif(g <= 0.0): gup = true;
+	if(b >= 1.0): bup = false;
+	elif(b <= 0.0): bup = true;
+	
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	
+	if(rup): r = r + delta + rng.randf_range(0, 0.05)
+	else: r = r - delta - rng.randf_range(0, 0.05)
+	if(gup): g = g + delta + rng.randf_range(0, 0.05)
+	else: g = g - delta - rng.randf_range(0, 0.05)
+	if(bup): b = b + delta + rng.randf_range(0, 0.05)
+	else: b = b - delta - rng.randf_range(0, 0.05)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):	
+	calcColors(delta)
+	# Incorrect point change here..
+	print(self.get_colors())
+	self.set_color(1, Color(r,g,b,1))
+	pass
