aa8a8bd9d1c3f25127ab8978c950cadb58c04b32
TurbaDux <jackperry2187@gmail.com>
2022-03-02 19:45:26 -0500

    flowing color cube
    Changes the spastic behavior of the previous implementation to be slightly more calming and flowy

--- src/colors.gd
+++ src/colors.gd
@@ -1,26 +1,38 @@
 extends Node
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# Declare member variables here.
+var r = 0.0; var rup = true;
+var g = 0.0; var gup = true;
+var b = 0.0; var bup = true;
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func calcColors(delta):
+	if(r >= 1.0): rup = false;
+	elif(r <= 0.0): rup = true;
+	if(g >= 1.0): gup = false;
+	elif(g <= 0.0): gup = true;
+	if(b >= 1.0): bup = false;
+	elif(b <= 0.0): bup = true;
+	
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	
+	if(rup): r = r + delta + rng.randf_range(0, 0.05)
+	else: r = r - delta - rng.randf_range(0, 0.05)
+	if(gup): g = g + delta + rng.randf_range(0, 0.05)
+	else: g = g - delta - rng.randf_range(0, 0.05)
+	if(bup): b = b + delta + rng.randf_range(0, 0.05)
+	else: b = b - delta - rng.randf_range(0, 0.05)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):	
-	if(delta % 20.0 == 0) :
-		var rng = RandomNumberGenerator.new()
-		rng.randomize()
-		var rand1 = rng.randf_range(0,1)
-		var rand2 = rng.randf_range(0,1)
-		var rand3 = rng.randf_range(0,1)
-		var rand4 = rng.randf_range(0,1)
-		var material = SpatialMaterial.new()
-		material.albedo_color = Color(rand1,rand2,rand3,rand4)
-		$MeshInstance.material_override = material	
+	calcColors(delta)
+	var material = SpatialMaterial.new()
+	material.albedo_color = Color(r,g,b,1)
+	$MeshInstance.material_override = material	
 	pass
