d2c10f4ee8c0231cbb13815e1d7ea50667a22775
jgerega107 <jakegerega107@gmail.com>
2022-03-23 19:32:20 -0400

    Added fullscreen capability as well as setting the initial screen mode

--- src/OptionsMenuScreen.gd
+++ src/OptionsMenuScreen.gd
@@ -1,26 +1,38 @@
 extends Control
 
-
+onready var screenmodes = get_node("VBoxContainer/HBoxContainer/VBoxContainer2/screenmode_options")
+onready var graphics = get_node("VBoxContainer/HBoxContainer/VBoxContainer/graphics_options")
+onready var resolutions = get_node("VBoxContainer/HBoxContainer/VBoxContainer2/res_options")
+	
 func _ready():
 	_prepare_screenmodes()
 	_prepare_graphics()
 	_prepare_resolutions()
+	if(!OS.window_fullscreen):
+		screenmodes.select(1)
 	
-
 func _prepare_screenmodes():
-	var screenmodes = get_node("HBoxContainer/VBoxContainer2/screenmode_options")
 	screenmodes.add_item("Fullscreen")
 	screenmodes.add_item("Windowed")
 	screenmodes.add_item("Borderless Fullscreen")
 	
 func _prepare_graphics():
-	var graphics = get_node("HBoxContainer/VBoxContainer/graphics_options")
 	graphics.add_item("High")
 	graphics.add_item("Medium")
 	graphics.add_item("Low")
 	
 func _prepare_resolutions():
-	var resolutions = get_node("HBoxContainer/VBoxContainer2/res_options")
 	resolutions.add_item("example 1920x1080")
 	resolutions.add_item("example 1600x900")
 	resolutions.add_item("example 1024x768")
+
+
+func _on_screenmode_options_item_selected(index):
+	if(index == 0):
+		OS.window_fullscreen = true;
+	elif(index == 1):
+		pass
+		#toggle Windowed
+	else:
+		pass
+		#toggle Borderless Windowed
