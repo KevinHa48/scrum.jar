b56082577e739ce239489a3a5f58fd865016acb8
jgerega107 <jakegerega107@gmail.com>
2022-03-24 21:00:48 -0400

    Fixed startup crash

--- src/Playername.gd
+++ src/Playername.gd
@@ -9,7 +9,7 @@ extends Label
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#self.text = global.playername
-	self.text = global.playername
+	self.text = "Default"
 	pass # Replace with function body.
 
 
