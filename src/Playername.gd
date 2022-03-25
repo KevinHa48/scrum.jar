1243ded2630a58aeedd6417eb03d9381eea702e9
Tyler Kaminski <durcor@disroot.org>
2022-03-24 21:05:23 -0400

    Update nickname when it's entered

--- src/Playername.gd
+++ src/Playername.gd
@@ -8,11 +8,9 @@ extends Label
 #onready var playername1: = $Playername
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#self.text = global.playername
-	self.text = global.playername
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	self.text = global.playername
