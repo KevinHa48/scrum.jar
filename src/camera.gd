7d7c182487d3c96a93e1e36c8a806b8dd522d443
liamsusername <lkpowp@gmail.com>
2022-04-06 21:11:52 -0400

    Fixed camera-based movement

--- src/camera.gd
+++ src/camera.gd
@@ -1,6 +1,6 @@
-extends Node
+extends Spatial
 
-
+export var sens := 0.1
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,13 +8,13 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_as_toplevel(true)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-var sens = 0.1
 var active = true
 func _input(event):
 	if event is InputEventMouseMotion and active:
