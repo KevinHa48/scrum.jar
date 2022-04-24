2b048544bb722d7b551e93474e775672d5cdc719
Tyler Kaminski <durcor@disroot.org>
2022-04-24 18:22:24 -0400

    Make music-adaptive block falling speed

--- src/fallingboxes.gd
+++ src/fallingboxes.gd
@@ -4,7 +4,7 @@ extends Spatial
 onready var _retical = $Player/SpringArm/OVRFirstPerson/Retical
 var fallingCube
 var rng
-var maxCubes = 200
+var maxCubes = 400
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -23,6 +23,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	deleteGroundCubes()
 	if not global.songplaying:
 		deleteAllCubes()
 		return
@@ -30,29 +31,19 @@ func _process(_delta):
 	var sum = 0
 	for n in global.histogram:
 		sum += n
-	if sum < 6:
+	if sum < 4.5:
 		return
-	# for cube in cubes:
-	# 	if cube.is_colliding():
-	#		cubes.remove(cubes.find(cube))
-	#		cube.queue_free()
 	if len(cubes) <= maxCubes:
 		var newCube = fallingCube.duplicate()
 		newCube.visible = true
 		newCube.translate(randomSpawn())
+		newCube.linear_velocity = Vector3(0, - sum * 1.5, 0)
 		newCube.set_gravity_scale(0.1)
 		newCube.contact_monitor = true
 		newCube.contacts_reported = 20
 		# newCube.connect("body_entered", self, "on_newCube_body_entered")
 		add_child(newCube)
 		cubes.append(newCube)
-	#else:
-		#var cube_to_delete = cubes[0]
-		#print(cube_to_delete.translation.y)
-		#cubes.remove(0)
-		#cube_to_delete.queue_free()
-	
-	deleteGroundCubes()
 
 func deleteGroundCubes():
 	for cube in cubes:
@@ -70,4 +61,4 @@ func deleteAllCubes():
 
 func randomSpawn():
 	rng.randomize()
-	return Vector3(rng.randf_range(-100, 100), 30, rng.randf_range(-100, 100))
+	return Vector3(rng.randf_range(-200, 200), 100, rng.randf_range(-200, 200))
